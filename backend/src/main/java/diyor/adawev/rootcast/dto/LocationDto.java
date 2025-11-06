24050c859622ac662048bde9a0c207d4d377cb2c
Diyor Adashev <adashevdiyor443@gmail.com>
2025-11-06 10:58:01 +0500

    Remove old subtree folders

--- backend/src/main/java/diyor/adawev/rootcast/dto/LocationDto.java
+++
@@ -1,6 +0,0 @@
-package diyor.adawev.rootcast.dto;
-
-public record LocationDto (String name,
-                           double lat,
-                           double lon){}
-
--- backend/src/main/java/diyor/adawev/rootcast/repository/RegisterRepo.java
+++
@@ -1,8 +0,0 @@
-package diyor.adawev.rootcast.repository;
-
-
-import diyor.adawev.rootcast.model.Register;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterRepo extends JpaRepository<Register, Long> {
-}
